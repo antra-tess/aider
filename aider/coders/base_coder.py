8702357a8e63c6aee32e54d5e09ad3bd785614e6
Tengro <tengro@lynxtrading.com>
2025-02-05 08:04:49 +0000

    refactor: Remove duplicate time import in base_coder.py

--- aider/coders/base_coder.py
+++ aider/coders/base_coder.py
@@ -14,6 +14,7 @@ import threading
 import time
 import traceback
 import webbrowser
+import time
 from collections import defaultdict
 from datetime import datetime
 from json.decoder import JSONDecodeError
